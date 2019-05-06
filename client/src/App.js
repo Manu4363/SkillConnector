1b9e80f1cfbd95811de6dbbfa1dbf50368341e53
unknown <e.amargier@gmail.com>
2019-05-06 14:02:30 +0200

    Add User Authentification (Login, Set Auth Token, Logout...)

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -12,8 +12,20 @@ import Alert from "./components/layout/Alert";
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
 
-const App = () => (
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => { 
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -29,5 +41,5 @@ const App = () => (
       </Fragment>
     </Router>
   </Provider>
-);
+)};
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({ 
       name: '',
       email: '',
@@ -22,8 +23,12 @@ const Register = ({ setAlert }) => {
       if (password !== password2){
         setAlert('Passwords do not match', 'danger');
       } else {
-
+        register({ name, email, password });
       }
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
   }
 
   return (
@@ -37,13 +42,12 @@ const Register = ({ setAlert }) => {
                 placeholder="Name" 
                 name="name" 
                 value={name} 
-                onChange={e => onChange(e)}
-                required 
+                onChange={e => onChange(e)} 
             />
         </div>
         <div className="form-group">
           <input type="email" placeholder="Email Address" name="email"  value={email} 
-                onChange={e => onChange(e)} required />
+                onChange={e => onChange(e)} />
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -54,7 +58,6 @@ const Register = ({ setAlert }) => {
             type="password"
             placeholder="Password"
             name="password"
-            minLength="6"
             value={password} 
             onChange={e => onChange(e)}
           />
@@ -64,7 +67,6 @@ const Register = ({ setAlert }) => {
             type="password"
             placeholder="Confirm Password"
             name="password2"
-            minLength="6"
             value={password2} 
             onChange={e => onChange(e)}
           />
@@ -79,7 +81,13 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
-export default connect(null, { setAlert})(Register)
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register)
