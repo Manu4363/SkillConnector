1c65a23c7dc8105620b901077d53026612a72782
unknown <e.amargier@gmail.com>
2019-05-06 09:55:19 +0200

    Landing & Navbar components + Login &  Register forms created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React, {Fragment} from 'react';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/layout/NavBar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => (
+  <Router>
     <Fragment>
-      <h1>App</h1>
+      <NavBar />
+      <Route exact path="/" component={Landing}></Route>
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </section>
     </Fragment>
+  </Router>
 );
 export default App;
