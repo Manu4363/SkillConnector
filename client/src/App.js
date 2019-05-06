2bda05486418e7bfb2346b6fc7de883c513bcf52
unknown <e.amargier@gmail.com>
2019-05-06 11:25:24 +0200

    Redux setup & Alerts

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//Components
 import NavBar from "./components/layout/NavBar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <NavBar />
-      <Route exact path="/" component={Landing}></Route>
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register}></Route>
-          <Route exact path="/login" component={Login}></Route>
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <Route exact path="/" component={Landing}></Route>
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 export default App;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,10 +6,9 @@ const Landing = () => {
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="x-large">Developer Connector</h1>
+          <h1 className="x-large">Skill Connector</h1>
           <p className="lead">
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
+            Create a professional profile/portfolio, share skills between other professionals
           </p>
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">Sign Up</Link>
